b2f3c6c16e859bdb9f295a1f5e0d6a8ef26a02db
himank jain <himank369123@gmail.com>
2023-10-05 20:31:58 +0530

    adding # router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
     ,
   </React.StrictMode>,
   document.getElementById('root')
